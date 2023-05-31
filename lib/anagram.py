b64a3c2ede7777eb20586c0731e1fa08a555b524
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-05-31 14:19:19 -0400

    Completes anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, match):
+        return [w for w in match if sorted(w) == sorted(self.word)]
